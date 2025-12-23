097576a149dcabaf9709e8087c1705e6a5dee8dc
BekhaDavlatov <166605638+BekhaDavlatov@users.noreply.github.com>
2025-12-23 11:23:17 +0100

    systemazing games folder and even file with games structures

---
+++ src/games/even.js
@@ -0,0 +1,11 @@
+const isEven = (num) => num % 2 === 0;
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const geneRaund = () => {
+    const question = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
+    return (question, correctAnswer);
+}
+
+export { description, geneRaund };

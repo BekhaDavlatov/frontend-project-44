c9766ddd348afda2ab8367dcf0d47ce305014424
BekhaDavlatov <166605638+BekhaDavlatov@users.noreply.github.com>
2025-12-24 13:13:20 +0100

    Another step of the project was passed brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import run from '../src/index.js';
+import generateRound from '../src/games/calc.js';
+
+const rule = 'What is the result of the expression?';
+run(rule, generateRound);
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function greetUser() {
+export const greetUser = () => {
   console.log('Welcome to the brain games');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Good time of the day, Mr.${name}`);

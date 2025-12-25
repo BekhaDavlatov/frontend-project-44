cf84cd67984f801c32c4ca8469a56d6fa3f499ef
BekhaDavlatov <166605638+BekhaDavlatov@users.noreply.github.com>
2025-12-25 02:29:26 +0100

    the last fixed fich

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetUser = () => {
+export default function greetUser() {
   console.log('Welcome to the brain games');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Good time of the day, Mr.${name}`);
+  return name;
 }

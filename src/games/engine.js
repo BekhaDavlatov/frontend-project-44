36bf92ce830d4a3d8f0e0ee22f0f06d6346eb71b
@BekhaDavlatov <davlatovbekhruz30@gmail.com>
2026-01-21 22:26:19 +0100

    last i hope

--- src/games/engine.js
+++ src/games/engine.js
@@ -3,9 +3,9 @@ import readlineSync from 'readline-sync';
 const roundsCount = 3;
 
 export default function runGame(description, generateRound) {
-  console.log('Welcome to the Brain Games');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  console.log(`Hello, ${name}!`);
   console.log(description);
 
   for (let i = 0; i < roundsCount; i++) {
@@ -15,7 +15,7 @@ export default function runGame(description, generateRound) {
 
     if (userAnswer !== round.answer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${round.answer}'.`);
-      console.log(`Lets try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
     console.log('Correct!');

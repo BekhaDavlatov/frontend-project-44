8563828d3c2ec9e68fdd307d46c1c9c0a1c940a5
@BekhaDavlatov <davlatovbekhruz30@gmail.com>
2026-01-21 22:14:18 +0100

    Hello changes

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default function greetUser() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Good time of the day, Mr.${name}`);
+  console.log(`Hello, ${name}`);
   return name;
 }
